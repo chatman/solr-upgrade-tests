8799bcda66cb88b866a4087c594f968404c2c8c6
Vivek <vivek.narang10@gmail.com>
2016-01-26 18:33:35 +0530

    Updating Code ...

--- src/main/java/org/apache/solr/tests/upgrade/interfaces/TestWorkFlow.java
+++
@@ -1,6 +0,0 @@
-package org.apache.solr.tests.upgrade.interfaces;
-
-public interface TestWorkFlow extends Messages {
-
-
-}
--- src/main/java/org/apache/solr/tests/upgrade/interfaces/Util.java
+++
@@ -1,9 +0,0 @@
-package org.apache.solr.tests.upgrade.interfaces;
-
-public interface Util {
-
-	public enum ReleaseNames { SOLR,ZOOKEEPER };
-	
-	public void postMessage(String message);
-	
-}
